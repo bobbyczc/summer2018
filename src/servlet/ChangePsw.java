9289d053c143bd41f3ae7fea9f8bf926a76f78dc
CZCFighting <1247405060@qq.com>
2018-08-01 16:53:26 +0800

    new

--- src/servlet/ChangePsw.java
+++ src/servlet/ChangePsw.java
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jdt.internal.compiler.apt.util.EclipseFileObject;
-
 import model.ResponseEntity;
 import service.UserService;
 
